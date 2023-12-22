e5df51c0bc6a95ecfb693cfed96cbbe19a2e3171
JulieCarpenter <108777878+JulieCarpenter@users.noreply.github.com>
2023-12-22 13:41:16 -0800

    Update src/data_check/test_data.py

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -59,7 +59,11 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
+def test_row_count(data):
+    assert 15000 < data.shape[0] < 1000000
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_price_range(data: pd.DataFrame, min_price: int, max_price: int):
+    """
+    Test that prices in data are within expected range (min_price, max_price)
+    """
+    assert all(data['price'].between(min_price, max_price))
